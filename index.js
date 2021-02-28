10c3e69abfaa20dfae17ef0d829f99dfec2b2a7e
Trijit Goswami <goswami.trijit@gmail.com>
2021-02-28 17:43:20 +0530

    init commit

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 const http = require('http');
-const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const app = express();
 
-const port = 3000;
 const host = "localhost";
 
-app.use(express.static('public'));
+const port = 5000;
 
-app.get("/", (req, res) => {
-  res.statusCode = 200;
-  res.sendFile(path.resolve(__dirname, 'public/index.html'));
-});
+const server = http.createServer((req, res) => {
+  if(req.method == 'GET') {
+    var file_url;
+    if(req.url == '/') {
+      file_url = '/index.html';
+    }
+    else {
+      file_url =  req.url;
+    }
 
-app.get("/contact", (req, res) => {
-    res.statusCode = 200;
-    res.sendFile(path.resolve(__dirname, 'public/contact.html'));
-  });
+    var file_path = path.resolve('./public' + file_url);
+    const file_ext = path.extname(file_path);
+    if(file_ext == '.html')
+    {
+       fs.exists(file_path, (exist) => {
+         if(!exist) {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end(`<html><body><h1><center>Error 404: ${file_url} not found! </center></h1></body></html>`);
+         }
+         else {
+           res.statusCode = 200;
+           res.setHeader('Content-Type', 'text/html');
+           fs.createReadStream(file_path).pipe(res);
+         }
+       });
+    }
+    else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(`<html><body><h1><center>Error 404: ${file_url} is not a html file! </center></h1></body></html>`);
+    }
+  }
 
-app.listen(port, host , (err) => {
-   console.log(`Server is running at port: ${port}`);
-});
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`<html><body><h1><center>Error 404: ${req.method} method is not supported! </center></h1></body></html>`);
+  }
+})
+
+server.listen(port, host, (err) => {
+  if(!err) {
+    console.log(`Server running at port: ${port}`);
+  }
+})
